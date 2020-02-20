d9ed3f3e145554f6b651d5cdfff0d77e67bc6ea7
Will Lewis <wlewis@spartaglobal.com>
2020-02-20 13:51:54 +0000

    created class PullCsv in file Dev_pull_csv to take csv files and output dataframe to adress used story pull csv

---
+++ Dev_pull_csv.py
@@ -0,0 +1,25 @@
+import boto3
+import pprint as pp
+from io import StringIO
+import pandas as pd
+import self as self
+
+
+class PullCsv:
+    def __init__(self, bucket): #when creating class, input bucket name as variable
+        self.s3_client = boto3.client('s3') #making client
+        s3_resource = boto3.resource('s3')
+        self.bucket2 = bucket
+        Contents = self.s3_client.list_objects(Bucket=bucket) #selecting bucket
+
+    def pull(self, folder, file): #use method pull on instance class PullCsv, specifying folder and filename
+        s3object = self.s3_client.get_object(Bucket=self.bucket2,Key=folder+'/'+file) #selecting file and folder
+
+        strbody = s3object['Body'].read() #reads body of object
+
+        str_body = strbody.decode('utf-8') #decodes field
+        StringData = StringIO(str_body)
+        df = pd.read_csv(StringData, sep=',') #turns into dataframe
+
+        return df
+
