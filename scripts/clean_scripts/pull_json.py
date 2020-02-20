44f9951694365680fc188c260acd45703ea5834d
Cassidy SCRUM Master <clouch@spartaglobal.com>
2020-02-20 16:18:41 +0000

    made the pull file classes for txt and json

--- scripts/clean_scripts/pull_json.py
+++ scripts/clean_scripts/pull_json.py
@@ -1,15 +1,16 @@
 import boto3
 import json
 
+
 # pulls file from aws when given the bucket name and outputs a dict
-class PullJson():
+class PullJson:
 
     def __init__(self, bucket):
         self._s3_client = boto3.client("s3")
         self._s3_resource = boto3.resource("s3")
         self._bucket = bucket
 
-    # reads the json file, ou
+    # take in file, and outputs a  json body
     def pull(self, folder, file):
         file = self._s3_client.get_object(Bucket=self._bucket, Key=folder + "/" + file)
         json_file = json.loads(file['Body'].read())
---
+++ scripts/clean_scripts/pull_txt.py
@@ -0,0 +1,50 @@
+import boto3
+import pandas as pd
+
+
+# setting the init for the class
+class PullTxt:
+    def __init__(self, bucket):
+        self._s3_client = boto3.client("s3")
+        self._s3_resource = boto3.resource("s3")
+        self._bucket = bucket
+
+    # pull file method
+    def pull(self, folder, file):
+
+        # setting the bucket as folder and file for scalability, and decoding the data so it is more usable
+        file = self._s3_client.get_object(Bucket=self._bucket, Key=folder + "/" + file)
+        file = file['Body'].read()
+        file = file.decode('utf-8')
+
+        # splits each line into a list
+        file_list = file.splitlines()
+
+        # taking data, as it was on the top row
+        date = file_list[0]
+
+        # taking the location the same way
+        location = file_list[1]
+
+        # the bulk of the data started from this point to the end, so each line is now a list
+        file_list_data = file_list[3:]
+
+        data_list = []
+        # making a for loop that splits each row into a list of lists
+        for line in file_list_data:
+            name = line[:line.find('-') - 1]
+            psychometrics = line[
+                            line.find('Psychometrics: ') + len('Psychometrics: '):line.find('/', line.find(
+                                'Psychometrics: '))]
+            presentation = line[
+                           line.find('Presentation: ') + len('Presentation: '):line.find('/',
+                                                                                         line.find('Presentation: '))]
+            # appending the list of lists into one
+            data_list.append([name, psychometrics, presentation, date, location])
+
+        # making the dafeframe
+
+        pd_columns = ['Name', 'Psychometrics', 'Presentation', 'Date', 'Location']
+        pd_format = pd.DataFrame(data_list, columns=pd_columns)
+        return pd_format
+
