4d8407a517ec3e8b178dbd46423aa63846b20470
Aaron McKenna <amckenna@spartaglobal.com>
2020-02-21 15:43:13 +0000

    Changes to aaron.py to append text file D7P-34 and start on append_txt - not ready to review

--- scripts/folder_scripts/test/aaron.py
+++ scripts/folder_scripts/test/aaron.py
@@ -1,10 +1,12 @@
+#### NEEDS TO IMPORT THE BELOW PullTxt to function:
 import boto3
 import pandas as pd
+from data7project.scripts.pull_scripts.pull_txt import PullTxt
 
-from data7project.pull_scripts.pull_txt import PullTxt
-
+######
+# CODE TO GENERATE DATE AND LOC of Sparta Days & Name/Pyscho/Present scores for attendees
 # create an instance of pull text class
-buckets = PullTxt("data7-engineering-project")
+data = PullTxt("data7-engineering-project")
 # create an instance of an S3 resource
 s3 = boto3.resource("s3")
 # create an instance of the Bucket
@@ -21,8 +23,17 @@ for object_summary in my_bucket.objects.filter(Prefix="SpartaDays/"):
 # iterate through the list of files, pull each as a df and append to previous
 for file in txt_files:
     if count == 0:
-        df = buckets.pull("SpartaDays", file)  # run the .pull method to bring data into df format from .txt
+        df = data.pull("SpartaDays", file)  # run the .pull method to bring data into df format from .txt
         count +=1  # increment count to ensure the df is not changed at the start of every iteration
     else:
-        df = df.append(buckets.pull("SpartaDays", file))  # append to existing df if not first iteration
+        sparta_days = df.append(data.pull("SpartaDays", file))  # append to existing df if not first iteration
 
+# return only the unique entries for both date & location
+date_loc = (sparta_days.groupby(['Date','Location']).size().reset_index())
+# drop the last column to display just date and location
+date_loc = date_loc.drop(date_loc.columns[-1], axis=1)
+# drop the Date & location columns to display just name, psycho and present scores
+name_psy_pres = sparta_days.drop(["Date", "Location"], axis=1)
+
+print(date_loc.head())
+print(name_psy_pres.head())
