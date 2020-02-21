3bf18cb0f7745b3c88f52521b3a4073370cae045
Will Lewis <wlewis@spartaglobal.com>
2020-02-21 10:35:22 +0000

    finished json scouring tool

--- tools/tools_json_scour.py
+++ tools/tools_json_scour.py
@@ -2,25 +2,24 @@ import boto3
 import json
 
 # pulls file from aws when given the bucket name and outputs a dict
-def Scour():
-    class PullJson2():
 
-        def __init__(self, bucket):
-            self._s3_client = boto3.client("s3")
-            self._s3_resource = boto3.resource("s3")
-            self._bucket = bucket
+class PullJson2():
+
+    def __init__(self, bucket):
+        self._s3_client = boto3.client("s3")
+        self._s3_resource = boto3.resource("s3")
+        self._bucket = bucket
 
         # reads the json file, ou
-        def pull(self, file):
-            file = self._s3_client.get_object(Bucket=self._bucket, Key= file)
-            json_file = json.loads(file['Body'].read())
-            return json_file
+    def pull(self, file):
+        file = self._s3_client.get_object(Bucket=self._bucket, Key= file)
+        json_file = json.loads(file['Body'].read())
+        return json_file
 
-    test = PullJson2('data7-engineering-project')
+
 
     def find(self,keyfactor):
         s3_client = boto3.client("s3")
-        s3_resource = boto3.resource('s3')
         contents = s3_client.list_objects(Bucket='data7-engineering-project')
         strengths = []
         for key in contents['Contents']:
@@ -30,5 +29,5 @@ def Scour():
                         strengths.append(x)
         return strengths
 
-look = Scour()
-look.
+test = PullJson2('data7-engineering-project')
+test.find('strengths')
