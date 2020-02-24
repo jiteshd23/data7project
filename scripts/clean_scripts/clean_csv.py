1fa32b1461a8c019a95be7c94789e962dfa6537a
Cassidy SCRUM Master <clouch@spartaglobal.com>
2020-02-24 14:28:51 +0000

    started making the functions for cleaning the tables

--- scripts/clean_scripts/clean_csv.py
+++ scripts/clean_scripts/clean_csv.py
@@ -1,12 +1,12 @@
 class MainClean:
     def __init__(self, table):
-        self.table
+        self.table = table
 
         # takes in a unclean table and converts nan to 0 and adds column at the end.
         # column is those who completed the course.
         # this should have return a clean table.
 
-    def performance_clean(self):
+    def performance_clean(self, df):
         fail = []
         for i in df[df.columns[-1]]:
             if i < 1:
