b99dbdafaf786ad27cf66e8d13801271ed897cfb
Cassidy SCRUM Master <clouch@spartaglobal.com>
2020-02-26 11:03:48 +0000

    Joining all 4 files

--- scripts/table_scripts/df_candidate_details.py
+++ scripts/table_scripts/df_candidate_details.py
@@ -6,66 +6,69 @@ from data7project.scripts.table_scripts.tools.append_tables_buckets import *
 from data7project.scripts.clean_scripts.clean_functions import *
 from data7project.tools.mispell_correct import *
 import pandas as pd
+from functools import reduce
 
 def clean_name(table,col):
-    table[col].map(lambda x: x.lower).map(lambda x: x.title)
+    table[col] = table[col].str.title()
     return table
 
-# talent = Append_All("data7-engineering-project").append_all("Talent")
-# sdays = Append_All("data7-engineering-project").append_all("SpartaDays")
-# academy = Append_All("data7-engineering-project").append_all("Academy")
+talent = Append_All("data7-engineering-project").append_all("Talent")
+sdays = Append_All("data7-engineering-project").append_all("SpartaDays")
+academy = Append_All("data7-engineering-project").append_all("Academy")
+sdays = sdays.rename(columns={"Name": "name"})
 # interview = pd.DataFrame.from_dict(Append_All("data7-engineering-project").append_all("Interview Notes"))
 
 
-talent = PullCsv("data7-engineering-project").pull("Talent", "April2019Applicants.csv")
-sdays = PullTxt("data7-engineering-project").pull("SpartaDays", "Sparta Day 10 April 2019.txt")
-academy = PullCsv("data7-engineering-project").pull("Academy","Business_24_2019-02-11.csv")
+# talent = PullCsv("data7-engineering-project").pull("Talent", "April2019Applicants.csv")
+# sdays = PullTxt("data7-engineering-project").pull("SpartaDays", "Sparta Day 10 April 2019.txt")
+# academy = PullCsv("data7-engineering-project").pull("Academy","Business_24_2019-02-11.csv")
 
-
+#
 talent = mispell(clean_name(talent, "name"), "name")
-sdays = mispell(clean_name(sdays, "Name"), "Name")
+sdays = mispell(clean_name(sdays, "name"), "name")
 academy = mispell(clean_name(academy, "name"), "name")
-talent = talent["name"]
-sdays = sdays["Name"]
-academy = academy["name"]
-
-
-name = talent.append(sdays).append(academy)
-# creates a frame of the series of just the cities
-frame = {"name": name}
-# create a dataframe for the cities
-people = pd.DataFrame(frame)
-# drop duplicates
-people = people.drop_duplicates('name')
-
-people.insert(0, "candidate_id", range(1, 1+len(people)))
-
-print(people["name"].values)
+# talent = talent["name"]
+# sdays = sdays["Name"]
+# academy = academy["name"]
+#
+#
+# name = talent.append(sdays).append(academy)
+# # creates a frame of the series of just the cities
+# frame = {"name": name}
+# # create a dataframe for the cities
+# people = pd.DataFrame(frame)
+# # drop duplicates
+# people = people.drop_duplicates('name')
+#
+# people.insert(0, "candidate_id", range(1, 1+len(people)))
+#
+# print(people["name"].values)
 
 
 
 def names_from_json(bucket):  # breaks down dataframe into only relevant information.
     test = PullSingle(bucket)
-    return test
-    print(test)
+    folder = "Interview Notes"
     _s3_client = boto3.client("s3")
     contents = _s3_client.list_objects(Bucket=bucket)
     dict_list = []
     outputs = []
     for key in contents['Contents']:
-        if 'Interview Notes' in key['Key']:
-            dict_list.append(test.pull('Interview Notes',key['Key'][len('Interview Notes')+1:]))
-    for values in dict_list:
-        for value in values['name']:
-            outputs.append([value['name']])
-    names = pd.DataFrame(outputs)
-    names.columns = ['name']
-    names = names.drop_duplicates('name')
-    return names
+        if folder in key['Key']:
+            dict_list.append(test.pull(folder, key['Key'][len(folder) + 1:]))
+    for value in dict_list:
+        n_list = [value['name'],value['date']]
+        outputs.append(n_list)
+    out = pd.DataFrame(outputs)
+    out.columns = ['name', 'date']
+    #out = out.drop_duplicates('name')
+    out = mispell(clean_name(out,"name"), "name")
+    return out
 
-print(names_from_json('data7-engineering-project'))
-
-
+interview = names_from_json('data7-engineering-project')
+data_frames = [talent, interview,sdays,academy]
+full = reduce(lambda left,right: pd.merge(left,right,on=['name'],how='outer'), data_frames)
+print(full.columns)
 #
 # # gender, inv date, dob, name email, phone, address, city, postcode, invited by, uni, degree
 # # need to add course schedual id, first name, last name, sparta day id, interview id
