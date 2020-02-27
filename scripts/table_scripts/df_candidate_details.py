bc3be5bf1e83a264b2154a45d864136668557356
Cassidy SCRUM Master <clouch@spartaglobal.com>
2020-02-27 10:40:36 +0000

    Finishing off last tables

--- scripts/table_scripts/df_candidate_details.py
+++ scripts/table_scripts/df_candidate_details.py
@@ -9,96 +9,90 @@ import pandas as pd
 from functools import reduce
 
 
-def clean_name(table,col):
+def clean_name(table, col):
     table[col] = table[col].str.title()
     return table
 
-talent = Append_All("data7-engineering-project").append_all("Talent")
-sdays = Append_All("data7-engineering-project").append_all("SpartaDays")
-academy = Append_All("data7-engineering-project").append_all("Academy")
-sdays = sdays.rename(columns={"Name": "name"})
-# interview = pd.DataFrame.from_dict(Append_All("data7-engineering-project").append_all("Interview Notes"))
+
+# talent = Append_All("data7-engineering-project").append_all("Talent")
+# sdays = Append_All("data7-engineering-project").append_all("SpartaDays")
+# academy = Append_All("data7-engineering-project").append_all("Academy")
+# sdays = sdays.rename(columns={"Name": "name"})
+# # interview = pd.DataFrame.from_dict(Append_All("data7-engineering-project").append_all("Interview Notes"))
+#
+#
+# # talent = PullCsv("data7-engineering-project").pull("Talent", "April2019Applicants.csv")
+# # sdays = PullTxt("data7-engineering-project").pull("SpartaDays", "Sparta Day 10 April 2019.txt")
+# # academy = PullCsv("data7-engineering-project").pull("Academy","Business_24_2019-02-11.csv")
+#
+# #
+# talent = mispell(clean_name(talent, "name"), "name")
+# sdays = mispell(clean_name(sdays, "name"), "name")
+# academy = mispell(clean_name(academy, "name"), "name")
+# # talent = talent["name"]
+# # sdays = sdays["Name"]
+# # academy = academy["name"]
+# #
+# #
+# # name = talent.append(sdays).append(academy)
+# # # creates a frame of the series of just the cities
+# # frame = {"name": name}
+# # # create a dataframe for the cities
+# # people = pd.DataFrame(frame)
+# # # drop duplicates
+# # people = people.drop_duplicates('name')
+# #
+# # people.insert(0, "candidate_id", range(1, 1+len(people)))
+# #
+# # print(people["name"].values)
+#
+#
+#
+# def names_from_json(bucket):  # breaks down dataframe into only relevant information.
+#     test = PullSingle(bucket)
+#     folder = "Interview Notes"
+#     _s3_client = boto3.client("s3")
+#     contents = _s3_client.list_objects(Bucket=bucket)
+#     dict_list = []
+#     outputs = []
+#     for key in contents['Contents']:
+#         if folder in key['Key']:
+#             dict_list.append(test.pull(folder, key['Key'][len(folder) + 1:]))
+#     for value in dict_list:
+#         n_list = [value['name'],value['date']]
+#         outputs.append(n_list)
+#     out = pd.DataFrame(outputs)
+#     out.columns = ['name', 'date']
+#     #out = out.drop_duplicates('name')
+#     out = mispell(clean_name(out,"name"), "name")
+#     return out
+#
+#
+# interview = names_from_json('data7-engineering-project')
+# data_frames = [talent, interview,sdays,academy]
+# full = reduce(lambda left,right: pd.merge(left,right,on=['name'],how='outer'), data_frames)
+# print(full.columns)
+# #
+# # # gender, inv date, dob, name email, phone, address, city, postcode, invited by, uni, degree
+# # # need to add course schedual id, first name, last name, sparta day id, interview id
+def candidate_details(bucket):
+    cand_id = Append_All(bucket).append_all("Talent")
+
+    # phone number needs the format changing, but in this situation our time can be better spent
+    cand_id = cand_id[
+        ["name", "id", "gender", "dob", "invited_date", "email", "phone_number", "address", "city", "invited_by", "uni",
+         "degree", "month"]]
+
+    cand_id["invited_date"] = cand_id["invited_date"].fillna(0)
+    cand_id["invited_date"] = cand_id["invited_date"].astype(int)
+    cand_id["invited_date"] = cand_id["invited_date"].astype(str)
+    cand_id["invite_date"] = cand_id["month"] + " " + cand_id["invited_date"]
+    cand_id["invite_date"] = cand_id["invite_date"].str.strip()
+    cand_id = (fix_date(cand_id, 'invite_date'))
+    cand_id = (clean_name(cand_id, 'name'))
+    cand_id["UNID"] = cand_id["name"] + cand_id["invite_date"]
+    cand_id = cand_id.drop(["invited_date", "month"], axis=1)
+    print(cand_id.columns)
 
 
-# talent = PullCsv("data7-engineering-project").pull("Talent", "April2019Applicants.csv")
-# sdays = PullTxt("data7-engineering-project").pull("SpartaDays", "Sparta Day 10 April 2019.txt")
-# academy = PullCsv("data7-engineering-project").pull("Academy","Business_24_2019-02-11.csv")
-
-#
-talent = mispell(clean_name(talent, "name"), "name")
-sdays = mispell(clean_name(sdays, "name"), "name")
-academy = mispell(clean_name(academy, "name"), "name")
-# talent = talent["name"]
-# sdays = sdays["Name"]
-# academy = academy["name"]
-#
-#
-# name = talent.append(sdays).append(academy)
-# # creates a frame of the series of just the cities
-# frame = {"name": name}
-# # create a dataframe for the cities
-# people = pd.DataFrame(frame)
-# # drop duplicates
-# people = people.drop_duplicates('name')
-#
-# people.insert(0, "candidate_id", range(1, 1+len(people)))
-#
-# print(people["name"].values)
-
-
-
-def names_from_json(bucket):  # breaks down dataframe into only relevant information.
-    test = PullSingle(bucket)
-    folder = "Interview Notes"
-    _s3_client = boto3.client("s3")
-    contents = _s3_client.list_objects(Bucket=bucket)
-    dict_list = []
-    outputs = []
-    for key in contents['Contents']:
-        if folder in key['Key']:
-            dict_list.append(test.pull(folder, key['Key'][len(folder) + 1:]))
-    for value in dict_list:
-        n_list = [value['name'],value['date']]
-        outputs.append(n_list)
-    out = pd.DataFrame(outputs)
-    out.columns = ['name', 'date']
-    #out = out.drop_duplicates('name')
-    out = mispell(clean_name(out,"name"), "name")
-    return out
-
-
-interview = names_from_json('data7-engineering-project')
-data_frames = [talent, interview,sdays,academy]
-full = reduce(lambda left,right: pd.merge(left,right,on=['name'],how='outer'), data_frames)
-print(full.columns)
-#
-# # gender, inv date, dob, name email, phone, address, city, postcode, invited by, uni, degree
-# # need to add course schedual id, first name, last name, sparta day id, interview id
-# cand_id = Append_All("data7-engineering-project").append_all("Talent")
-#
-#
-# # phone number needs the format changing, but in this situation are time can be better spent
-# cand_id = cand_id[
-#     ["id", "gender", "dob", "invited_date", "email", "phone_number", "address", "invited_by", "uni", "degree", "month"]]
-#
-# cand_id["invited_date"] = cand_id["invited_date"].fillna(0)
-# cand_id["invited_date"] = cand_id["invited_date"].astype(int)
-# cand_id["invited_date"] = cand_id["invited_date"].astype(str)
-# cand_id["invite_date"] = cand_id["month"] + " " + cand_id["invited_date"]
-# cand_id["invite_date"] = cand_id["invite_date"].str.strip()
-# cand_id = (fix_date(cand_id, 'invite_date'))
-# # print(cand_id)
-#
-# cand_id2 = Append_All("data7-engineering-project").append_all("SpartaDays")
-#
-# cand_name = cand_id2[
-#     ["Name"]
-# ]
-# # print(cand_id2.loc[cand_id2["Name"]=="Bett"])
-#
-#
-# # candidate = pd.concat([cand_id, cand_name], axis=1)
-# # print(candidate)
-#
-# test = clean_name(cand_id,"name")
-# print(test)
+candidate_details("data7-engineering-project")
--- scripts/table_scripts/df_performance_all.py
+++ scripts/table_scripts/df_performance_all.py
@@ -1,40 +1,59 @@
 from data7project.scripts.table_scripts.tools.append_tables_buckets import *
-from data7project.tools.mispell_correct import *
+from data7project.scripts.clean_scripts.clean_functions import *
+
 
 """ This is the creation of the performance tables, where the data will be split by rubrick and contained in different 
 tables.
 performanceAll is a class that will take all the files from the academy bucket and clean the names of the students 
 simultaneously, split by rubrick and append onto its individual dataframe """
+# to merge on name and 2+ scores. date is in the file title, we know how to do it but are pressed for time
 class PerformanceAll:
     def __init__(self, bucket):
-        self._appended_acad_df = mispell(Append_All(bucket).append_all('Academy'), 'name')
+        self._appended_acad_df = Append_All(bucket).append_all('Academy', include_title=1), 'name'
+        # new data frame with split value columns on the underscore
+        new = self._appended_acad_df["title"].str.split("_", n=2, expand=True)
+        # making separate course name column from new data frame
+        self._appended_acad_df["course"] = new[0]
+        # making separate course name column from new data frame
+        self._appended_acad_df["course_number"] = new[1]
+        # making separate start date column from new data frame
+        self._appended_acad_df["start_date"] = new[2]
+        # Dropping old title columns
+        self._appended_acad_df.drop(columns=["title"], inplace=True)
+        # remove .csv from the filename in the start_date column
+        self._appended_acad_df["start_date"] = self._appended_acad_df["start_date"].str.replace('.csv', '')
+        self._appended_acad_df = fix_date(self._appended_acad_df, "start_date")
+        # returns an ordered list of unique courses from all csvs using course and course number
+        self._appended_acad_df = self._appended_acad_df.sort_values(['course', "course_number"], ascending=True)
+        # returns only name and start_date
+        self._appended_acad_df = self._appended_acad_df[["name", "start_date"]]
 
     def ih_pull(self):
         ih_pull = self._appended_acad_df.filter(
-            ["name", "IH_W1", "IH_W2", "IH_W3", "IH_W4", "IH_W5", "IH_W6", "IH_W7", "IH_W8", "IH_W9", "IH_W10"])
-        return ih_pull
+            ["name", "start_date", "IH_W1", "IH_W2", "IH_W3", "IH_W4", "IH_W5", "IH_W6", "IH_W7", "IH_W8", "IH_W9", "IH_W10"])
+        return clean_name(ih_pull, "name")
 
     def is_pull(self):
         is_pull = self._appended_acad_df.filter(
-            ["name", "IS_W1", "IS_W2", "IS_W3", "IS_W4", "IS_W5", "IS_W6", "IS_W7", "IS_W8", "IS_W9", "IS_W10"])
-        return is_pull
+            ["name","start_date", "IS_W1", "IS_W2", "IS_W3", "IS_W4", "IS_W5", "IS_W6", "IS_W7", "IS_W8", "IS_W9", "IS_W10"])
+        return clean_name(is_pull, "name")
 
     def pv_pull(self):
         pv_pull = self._appended_acad_df.filter(
-            ["name", "PV_W1", "PV_W2", "PV_W3", "PV_W4", "PV_W5", "PV_W6", "PV_W7", "PV_W8", "PV_W9", "PV_W10"])
-        return pv_pull
+            ["name","start_date", "PV_W1", "PV_W2", "PV_W3", "PV_W4", "PV_W5", "PV_W6", "PV_W7", "PV_W8", "PV_W9", "PV_W10"])
+        return clean_name(pv_pull,"name")
 
     def ps_pull(self):
         ps_pull = self._appended_acad_df.filter(
-            ["name", "PS_W1", "PS_W2", "PS_W3", "PS_W4", "PS_W5", "PS_W6", "PS_W7", "PS_W8", "PS_W9", "PS_W10"])
-        return ps_pull
+            ["name", "start_date","PS_W1", "PS_W2", "PS_W3", "PS_W4", "PS_W5", "PS_W6", "PS_W7", "PS_W8", "PS_W9", "PS_W10"])
+        return clean_name(ps_pull,"name")
 
     def sd_pull(self):
         sd_pull = self._appended_acad_df.filter(
-            ["name", "SD_W1", "SD_W2", "SD_W3", "SD_W4", "SD_W5", "SD_W6", "SD_W7", "SD_W8", "SD_W9", "SD_W10"])
-        return sd_pull
+            ["name","start_date", "SD_W1", "SD_W2", "SD_W3", "SD_W4", "SD_W5", "SD_W6", "SD_W7", "SD_W8", "SD_W9", "SD_W10"])
+        return clean_name(sd_pull,"name")
 
     def sa_pull(self):
         sa_pull = self._appended_acad_df.filter(
-            ["name", "SA_W1", "SA_W2", "SA_W3", "SA_W4", "SA_W5", "SA_W6", "SA_W7", "SA_W8", "SA_W9", "SA_W10"])
-        return sa_pull
+            ["name", "start_date","SA_W1", "SA_W2", "SA_W3", "SA_W4", "SA_W5", "SA_W6", "SA_W7", "SA_W8", "SA_W9", "SA_W10"])
+        return clean_name(sa_pull,"name")
